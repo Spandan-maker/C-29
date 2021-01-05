7c14d9d82c9b48ad2779c8390489ca3d64df8204
Spandan <74659727+Spandan-maker@users.noreply.github.com>
2021-01-05 20:26:33 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,24 @@
+class Bird{
+  constructor(x, y, r, angle) {
+      var options = {
+          'restitution':0.8,
+          'friction':1.0,
+          'density':1.0
+      }
+      this.body = Bodies.circle(x, y, r, options);
+      this.r = r;
+
+      this.image = loadImage("sprites/bird.png");
+      World.add(world, this.body);
+    }
+    display(){
+      var angle = this.body.angle;
+
+      push();
+      translate(this.body.position.x, this.body.position.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.width, this.height);
+      pop();
+    }
+}
